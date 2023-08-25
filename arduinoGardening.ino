ad05806c36e5b130be45ee6bd61fe828c3611e2e
Snugant <143151242+Snugant@users.noreply.github.com>
2023-08-25 12:52:26 +1000

    Update arduinoGardening.ino

--- arduinoGardening.ino
+++ arduinoGardening.ino
@@ -1,29 +1,24 @@
-int dir1PinA = 6;
-
-int dir2PinA = 5;
-
-
-
-
+const int dir1PinA = 7;
+const int dir2PinA = 6;
+const int dir3PinA = 5;
+const int dir4PinA = 4;
+const int enA = 3;
+int count = 100;
 
 void setup() {
-
   pinMode(dir1PinA, OUTPUT);
-
   pinMode(dir2PinA, OUTPUT);
-
-
-
+  pinMode(dir3PinA, OUTPUT);
+  pinMode(dir4PinA, OUTPUT);
+  pinMode(enA, OUTPUT);
   digitalWrite(dir1PinA, LOW);
-
   digitalWrite(dir2PinA, HIGH);
-
+  digitalWrite(dir3PinA, LOW);
+  digitalWrite(dir4PinA, HIGH);
+  Serial.begin(9600);
 }
-
-
-
 void loop() {
-
-
+  analogWrite(enA, count);
+  Serial.println(count);
 
 }

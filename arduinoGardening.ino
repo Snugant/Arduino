db7b4d7d5648e9c91cc9a5e422aa4e2aabb80167
Snugant <143151242+Snugant@users.noreply.github.com>
2023-08-25 12:51:36 +1000

    Add files via upload

---
+++ arduinoGardening.ino
@@ -0,0 +1,29 @@
+int dir1PinA = 6;
+
+int dir2PinA = 5;
+
+
+
+
+
+void setup() {
+
+  pinMode(dir1PinA, OUTPUT);
+
+  pinMode(dir2PinA, OUTPUT);
+
+
+
+  digitalWrite(dir1PinA, LOW);
+
+  digitalWrite(dir2PinA, HIGH);
+
+}
+
+
+
+void loop() {
+
+
+
+}
